974c19cce179429b54a3421da7c43fc2c9af2e61
Vicente Lucas <vica.escorcio@gmail.com>
2017-05-22 15:51:30 -0300

    first MVP

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.flip').click(function(){
-		var id = $(this).parent().attr("id");
+		var id = $(this).parent().attr("href");
 		$(this).parent().hide("drop", {}, 1000, function(){
 			$(id).show("drop",{},1000);
 		});
